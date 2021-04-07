03d4573464e0722236764ae67b0f959f164a9569
Kurt Simmons <19kds98@gmail.com>
2021-04-06 20:11:06 -0700

    learning basic notation

---
+++ javascript/scripts.js
@@ -0,0 +1,33 @@
+// primitive data types
+const no = false;
+const yes = true;
+const count = 9;
+const strings = 'abc';
+
+let score = 0;
+score = 10.5;
+
+
+//complex data types ( groups )
+const object = {
+    no : false,
+    yes : true,
+    number: 1,
+    strings : 'abc',
+}
+
+
+//array notation
+const array = [false, true, 1, 'abc', object];
+
+
+//dot notation
+alert(object.number);
+
+//bracket notation
+alert(object['number'])
+
+
+
+
+//to troubleshoot an error, inspect your browser and click on the console tab which will then display the error. It will show you the exact line the error is found
